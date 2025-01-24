12abac1f188d389aa2a253d93c64bdf8675f06ce
eghosa02 <pezzoloedoardo@gmail.com>
2025-01-24 15:57:05 +0100

    json dati da tabellare

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from view.home_view import Home_view
+from view.test_view import Test_view
+
+app = Flask(__name__)
+
+Home_view.register(app)
+Test_view.register(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
